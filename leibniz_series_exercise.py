957412c36ec475128a269bfeed06a1dda1eaff03
noyagross123 <noyagross1@gmail.com>
2025-11-09 19:14:13 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,6 +1,6 @@
 def approximate_pi(n_terms):
   total = 0.0
   for i in range (n_terms):
-      term=(-1.0)**i/(2*i+1)
-      total+=term
+      term =(-1.0)**i/(2*i+1)
+      total += term
   return 4.0 * total
